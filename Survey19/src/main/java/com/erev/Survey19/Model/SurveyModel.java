c49f5310c3f3dc919530a3ae7983e04b498af8b0
RamitKundu <ramitkundu93@gmail.com>
2020-04-21 23:37:46 +0530

    Model class commit

---
+++ Survey19/src/main/java/com/erev/Survey19/Model/SurveyModel.java
@@ -0,0 +1,41 @@
+package com.erev.Survey19.Model;
+
+public class SurveyModel {
+
+		
+		private String name;
+		private String address;
+		private int age;
+		private String problemFaced;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getProblemFaced() {
+			return problemFaced;
+		}
+		public void setProblemFaced(String problemFaced) {
+			this.problemFaced = problemFaced;
+		}
+		
+		
+		
+		
+
+	
+
+}
